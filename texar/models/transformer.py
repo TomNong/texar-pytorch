87f1db88c89a604e49f2198ce8dda48b34b74696
Allen Shi <haoranshi97@gmail.com>
2019-06-06 14:18:25 -0400

    Work on decoding

--- texar/models/transformer.py
+++ texar/models/transformer.py
@@ -139,7 +139,7 @@ class Transformer(ModuleBase):
 
             def _embedding_fn(x, y):
                 x = x.to(device=self.word_embedder._embedding.device)
-                y = y.to(device=self.pos_embedder._embedding.device)
+                y = y.to(device=x.device)
                 return self.word_embedder(x.type(torch.long)) \
                     * self.config_model.hidden_dim ** 0.5\
                     + self.pos_embedder(y.type(torch.long))
